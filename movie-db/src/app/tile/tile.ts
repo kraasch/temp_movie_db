ec1e1c35edd532df4161c26a15b8362198845449
Alex Kraasch <alex@kraasch.eu>
2026-03-02 15:53:03 +0100

    Pass in titles to inner component.

--- movie-db/src/app/tile/tile.ts
+++ movie-db/src/app/tile/tile.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tile',
@@ -26,5 +26,5 @@ import { Component } from '@angular/core';
 `,
 })
 export class Tile {
-  title: string = "Toast";
+  @Input() title: string = "";
 }
